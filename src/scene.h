a827f136245e13e202274c443b98d8b15a61d9e5
smolen92 <edo234@gmail.com>
2025-09-03 14:53:42 +0200

    comments

--- src/scene.h
+++ src/scene.h
@@ -10,10 +10,30 @@
 
 class Scene {
 	public:
+		/**
+		 * @param b pointer to button, that will be added to scene
+		 */
 		void add_button(Button* b);
+		
+		/**
+		 * @brief will add text into the scene
+		 *
+		 * @param t text itself
+		 * @param c color of the text
+		 */
 		void add_text(const char* t, SDL_Color c);
+		
+		/**
+		 * @brief same as button check_input
+		 */
 		void check_input(float mousex, float mousey, bool LMB_state);
+		
+		/**
+		 * @param renderer renderer that will be used for rendering
+		 * @param font font for the text
+		 */
 		void render(SDL_Renderer* renderer, TTF_Font* font);
+		
 		~Scene();
 	private:
 		std::vector<Button*> buttons;
